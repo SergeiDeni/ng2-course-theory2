fd63a95d514f44662308ffe19fb9c327bb69ab19
Sergej <se.dzenisevich@gmail.com>
2019-06-11 12:24:42 +0300

    test 3

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,28 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: `./car.component.html`,
+  //     `
+  // <h2>Single Car</h2>
+  //     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, soluta!</p>
+  // `,
+
+  //styleUrls: ['./car.component.css']
+  styles: [`
+  h2 {
+    color: aqua;
+  }
+  `]
+})
+export class CarComponent {
+
+        carName = 'Ford';
+        carYear = 2017;
+
+        getName(){
+            return this.carName;
+        }
+
+  }
+
